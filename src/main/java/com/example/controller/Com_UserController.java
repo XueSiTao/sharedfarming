40789b78ac9dd9e66999d5da620710aecd01bd7f
XueSiTao <2422739079@qq.com>
2018-12-18 16:32:55 +0800

    1

--- src/main/java/com/example/controller/Com_UserController.java
+++ src/main/java/com/example/controller/Com_UserController.java
@@ -1,7 +1,8 @@
 package com.example.controller;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,18 +14,16 @@ import com.example.service.Com_UserService;
 @RequestMapping("/")
 public class Com_UserController {
 
-	@Autowired
-	private static Com_UserService userService;
+	@Resource
+	private Com_UserService userService;
 	
 	
 	@RequestMapping("/login")
 	@ResponseBody
 	public String HellowWord(String bindKeyInput1,String bindKeyInput2){
 		Com_User user= new Com_User();
-		user.setLoginname("admin");
-		user.setLoginpass("admin");
-//		user.setLoginname(bindKeyInput1);
-//		user.setLoginpass(bindKeyInput2);
+		user.setLoginname(bindKeyInput1);
+		user.setLoginpass(bindKeyInput2);
 		int cou=userService.loginReception(user);
 		return cou+"";
 	}

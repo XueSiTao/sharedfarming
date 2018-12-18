916e87e519049f8e992a6f1c2120f2ecf09d22e4
XueSiTao <2422739079@qq.com>
2018-12-18 16:14:33 +0800

    1

--- src/main/java/com/example/controller/Com_UserController.java
+++ src/main/java/com/example/controller/Com_UserController.java
@@ -14,26 +14,20 @@ import com.example.service.Com_UserService;
 public class Com_UserController {
 
 	@Autowired
-	private static Com_UserService userService;
+	private Com_UserService userService;
 	
 	
 	@RequestMapping("/login")
 	@ResponseBody
 	public String HellowWord(String bindKeyInput1,String bindKeyInput2){
 		Com_User user= new Com_User();
-		user.setLoginname(bindKeyInput1);
-		user.setLoginpass(bindKeyInput2);
+		user.setLoginname("admin");
+		user.setLoginpass("admin");
+//		user.setLoginname(bindKeyInput1);
+//		user.setLoginpass(bindKeyInput2);
 		userService.loginReception(user);
 		return null;
 	}
 	
 	
-	
-	public static void main(String[] args) {
-		Com_User user= new Com_User();
-		user.setLoginname("admin");
-		user.setLoginpass("admin");
-		int cou=userService.loginReception(user);
-		System.err.println(cou);
-	}
 }

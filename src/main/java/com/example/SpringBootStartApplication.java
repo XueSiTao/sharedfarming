752497ca498e1fbc4fed4d7c754e619c86a896e5
XueSiTao <2422739079@qq.com>
2018-12-18 10:22:46 +0800

    1

--- src/main/java/com/example/SpringBootStartApplication.java
+++ src/main/java/com/example/SpringBootStartApplication.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
@@ -10,16 +9,12 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 
 public class SpringBootStartApplication extends SpringBootServletInitializer {
- 
-      public static void main( String[] args ){
-    	SpringApplication.run(SpringBootStartApplication .class, args);
-}
      /**
       *新增此方法
       */
       @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         // 注意这里要指向原先用main方法执行的Application启动类
-        return builder.sources(SpringBootStartApplication .class);
+        return builder.sources(SharedfarmingApplication .class);
     }
 }
